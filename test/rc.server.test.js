d5408780b00959582f7ec3d72909cd9d9088b529
Simon Petrac <pfoerdie@gmail.com>
2023-11-22 13:53:55 +0100

    added server test

---
+++ test/rc.server.test.js
@@ -0,0 +1,25 @@
+const
+    {describe, test, before} = require('mocha'),
+    expect                   = require('expect');
+
+describe('fua.app.rc.server', function () {
+
+    test('alice.tb.nicos-rd.com', async function () {
+        const response = await fetch('https://alice.tb.nicos-rd.com/about');
+        expect(response.ok).toBeTruthy();
+        const result = await response.json();
+        expect(result).toMatchObject({
+            issuer: 'https://alice.tb.nicos-rd.com/'
+        });
+    });
+
+    test('bob.tb.nicos-rd.com', async function () {
+        const response = await fetch('https://bob.tb.nicos-rd.com/about');
+        expect(response.ok).toBeTruthy();
+        const result = await response.json();
+        expect(result).toMatchObject({
+            issuer: 'https://bob.tb.nicos-rd.com/'
+        });
+    });
+
+});
