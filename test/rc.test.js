2e56ec9704d92912f69c71d8568c122c8087ae80
Simon Petrac <pfoerdie@gmail.com>
2023-11-22 13:48:02 +0100

    added dummy test

---
+++ test/rc.test.js
@@ -0,0 +1,11 @@
+const
+    {describe, test, before} = require('mocha'),
+    expect                   = require('expect');
+
+describe('fua.app.rc', function () {
+
+    test('develop', async function () {
+        // TODO
+    });
+
+});
